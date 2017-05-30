6bc8219dcf5190f97ec11a8dc203ddc46576e9b1
dudinam <namdd@vasco.com.vn>
2017-05-30 23:11:38 +0700

    Update binaries_java.sh

--- lib/binaries_java.sh
+++ lib/binaries_java.sh
@@ -2,7 +2,7 @@ install_java() {
   local version="$1"
   local dir="$2"
   
-  local download_url="http://ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-8u74-linux-x64.tar.gz"
+  local download_url="http://ftp.wsisiz.edu.pl/pub/pc/pozyteczne%20oprogramowanie/java/8u73/jdk-8u73-linux-x64.tar.gz"
   echo "Downloading JAVA [$download_url]"
   curl  --silent --fail --retry 5 --retry-max-time 15 -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "$download_url" -o /tmp/java.tar.gz || (echo "Unable to download java; does it exist?" && false)
   echo "Download complete!"
